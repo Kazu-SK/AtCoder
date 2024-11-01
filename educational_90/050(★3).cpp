e2c25d673bdc642be8c47748c64d83485856f41c
Kazu-SK <sonodans109@gmail.com>
2024-11-01 23:38:52 +0900

    add educational_90 50

---
+++ educational_90/050(★3).cpp
@@ -0,0 +1,38 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+int main()
+{
+    int N = 0;
+    int L = 0;
+    LL DIV = 1e9 + 7;
+
+
+    cin >> N >> L;
+
+    vector<LL> dp(N + 1);
+
+    dp[0] = 1;
+
+    for (int i = 1; i <= N; i++) {
+
+        if (i - L < 0)
+            dp[i] = dp[i - 1];
+        else
+            dp[i] = (dp[i - 1] + dp[i - L]) % DIV;
+    }
+
+    cout << dp[N] << endl;
+
+    return 0;
+}
+
