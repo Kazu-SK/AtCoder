065b53b65264e7416ea5ec1b9b0de16530e0fe3f
Kazu-SK <sonodans109@gmail.com>
2024-11-02 10:49:42 +0900

    add educational_90 52

---
+++ educational_90/052(★3).cpp
@@ -0,0 +1,44 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+int main()
+{
+    int N = 0;
+    LL ans = 1;
+    const LL DIV = 1e9 + 7;
+
+
+    cin >> N;
+
+    vector<vector<int>> A(N, vector<int>(6));
+    vector<int> sum(N);
+
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < 6; j++) {
+            cin >> A[i][j];
+
+            sum[i] += A[i][j];
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        
+        ans *= sum[i];
+        ans %= DIV;
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
+
