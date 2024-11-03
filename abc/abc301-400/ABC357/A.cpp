4f99ce3eb60bfdfd0ef4828c00cac32f4f5f90f8
Kazu-SK <sonodans109@gmail.com>
2024-11-03 16:42:24 +0900

    add abc357 A

---
+++ abc/abc301-400/ABC357/A.cpp
@@ -0,0 +1,39 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int N = 0, M = 0;
+
+
+    cin >> N >> M;
+
+    vector<int> H(N, 0);
+
+    for (int i = 0; i < N; i++) {
+        cin >> H[i];
+    }
+
+    int arian_hand_count = 0;
+
+    for (int i = 0; i < N; i++) {
+
+        if (M - H[i] >= 0) {
+            arian_hand_count++;
+            M -= H[i];
+        }
+        else {
+            break;
+        }
+    }
+
+    cout << arian_hand_count << endl;
+
+
+    return 0;
+}
+
