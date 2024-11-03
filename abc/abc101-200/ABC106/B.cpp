284782f2caaa8f95d895ecbe608d77ee10833ef1
Kazu-SK <sonodans109@gmail.com>
2024-11-03 10:18:27 +0900

    add abc106

---
+++ abc/abc101-200/ABC106/B.cpp
@@ -0,0 +1,42 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+int main()
+{
+    int N = 0;
+    int ans = 0;
+
+
+    cin >> N;
+
+    for (int n = 1; n <= N; n++) {
+
+        int count = 0;
+
+        if (n % 2 == 0)
+            continue;
+
+        for (int div = 1; div <= N; div++) {
+            if (n % div == 0) {
+                count++;
+            }
+        }
+
+        if (count == 8)
+            ans++;
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
+
