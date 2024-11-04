b4fb978c975e7c2883e612cb646375847d199398
Kazu-SK <sonodans109@gmail.com>
2024-11-04 11:50:34 +0900

    add educational_90 79

--- educational_90/079(★3).cpp
+++ educational_90/079(★3).cpp
@@ -24,52 +24,52 @@ int main()
     int range[4][2] = {{0,0},{0,1},{1,1},{1,0}};
 
 
-    for (int h = 0; h < H; h++) {
-        for (int w = 0; w < W; w++)
-            cin >> A[h][w];
+	for (int h = 0; h < H; h++) {
+		for (int w = 0; w < W; w++)
+			cin >> A[h][w];
 	}
 
-    for (int h = 0; h < H; h++) {
-        for (int w = 0; w < W; w++)
-            cin >> B[h][w];
+	for (int h = 0; h < H; h++) {
+		for (int w = 0; w < W; w++)
+			cin >> B[h][w];
 	}
 
     LL ans = 0;
 
-    for (int h = 0; h < H - 1; h++) {
-        for (int w = 0; w < W - 1; w++) {
+	for (int h = 0; h < H - 1; h++) {
+		for (int w = 0; w < W - 1; w++) {
 
 			int diff = B[h][w] - A[h][w];
 			ans += abs(diff);
 
-            for (int d = 0; d < 4; d++) {
-                int d_h = 0; 
-                int d_w = 0; 
+			for (int d = 0; d < 4; d++) {
+				int d_h = 0; 
+				int d_w = 0; 
 
-                d_h = range[d][0];
-                d_w = range[d][1];
+				d_h = range[d][0];
+				d_w = range[d][1];
 
-                A[h + d_h][w + d_w] += diff;
-            }
-        }
-    }
+				A[h + d_h][w + d_w] += diff;
+			}
+		}
+	}
 
     bool judge = true;
 
-    for (int h = 0; h < H; h++) {
-        for (int w = 0; w < W; w++){
-            if (A[h][w] != B[h][w])
-                judge = false;
-        }
+	for (int h = 0; h < H; h++) {
+		for (int w = 0; w < W; w++){
+			if (A[h][w] != B[h][w])
+				judge = false;
+		}
 	}
 
-    if (judge) {
-        cout << "Yes" << endl;
-        cout << ans << endl;
-    }
-    else
-        cout << "No" << endl;
+	if (judge) {
+		cout << "Yes" << endl;
+		cout << ans << endl;
+	}
+	else
+		cout << "No" << endl;
 
-    return 0;
+	return 0;
 }
 
