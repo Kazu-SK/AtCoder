892699f271da59b92a9da9f9da3c43ae1f746ff4
Kazu-SK <sonodans109@gmail.com>
2024-11-09 13:58:47 +0900

    add abc128 c

--- abc/abc101-200/ABC128/C.cpp
+++ abc/abc101-200/ABC128/C.cpp
@@ -12,62 +12,62 @@ using LL = long long;
 
 int main()
 {
-    int N = 0;
-    int M = 0;
+	int N = 0;
+	int M = 0;
 
 
-    cin >> N >> M;
+	cin >> N >> M;
 
-    vector<int> k(M);
-    vector<vector<int>> s(M);
-    vector<int> p(M);
-    vector<int> k_s_num(M);
+	vector<int> k(M);
+	vector<vector<int>> s(M);
+	vector<int> p(M);
+	vector<int> k_s_num(M);
 
 
-    for (int i = 0; i < M; i++) {
-        cin >> k[i];
+	for (int i = 0; i < M; i++) {
+		cin >> k[i];
 
-        for (int j = 0; j < k[i]; j++) {
-            int s_num = 0;
-            cin >> s_num;
-            s_num--;
+		for (int j = 0; j < k[i]; j++) {
+			int s_num = 0;
+			cin >> s_num;
+			s_num--;
 
-            k_s_num[i] += (1 << s_num);
-        }
-    }
+			k_s_num[i] += (1 << s_num);
+		}
+	}
 
-    for (int i = 0; i < M; i++)
-        cin >> p[i];
+	for (int i = 0; i < M; i++)
+		cin >> p[i];
 
-    int ans = 0;
+	int ans = 0;
 
 	for (int num = 0; num < (1 << N) ; num++) {
 
-        int light_on = 0;
+		int light_on = 0;
 
-        for (int i = 0; i < M; i++) {
+		for (int i = 0; i < M; i++) {
 
-            int s_on = num & k_s_num[i];
-            int s_on_num = 0;
+			int s_on = num & k_s_num[i];
+			int s_on_num = 0;
 
-            for (int bit = 0; bit < N; bit++) {
-                if (s_on & (1 << bit))
-                    s_on_num++;
-            }
-            
-            if (p[i] == s_on_num % 2) {
-                light_on++;
-            }
-        }
+			for (int bit = 0; bit < N; bit++) {
+				if (s_on & (1 << bit))
+					s_on_num++;
+			}
+			
+			if (p[i] == s_on_num % 2) {
+				light_on++;
+			}
+		}
 
-        if (light_on == M) {
-            ans++;
-        }
+		if (light_on == M) {
+			ans++;
+		}
 	}
 
-    cout << ans << endl;
+	cout << ans << endl;
 
-    
-    return 0;
+
+	return 0;
 }
 
