9f896e0711d5fcab41a884ab4d3feaed0cecc723
Kazu-SK <sonodans109@gmail.com>
2024-11-10 12:12:08 +0900

    add tessoku 5.1

---
+++ kyopro-tessoku/5.1/A26.cpp
@@ -0,0 +1,43 @@
+﻿
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int main()
+{
+    int Q = 0;
+
+
+    cin >> Q;
+
+    vector<int> X(Q, 0);
+
+
+    for (int i = 0; i < Q; i++)
+        cin >> X[i];
+
+
+
+    for (int i = 0; i < Q; i++) {
+
+        bool answer = true;
+
+        for (int j = 2; j * j <= X[i]; j++) {
+            if (X[i] % j == 0) {
+                answer = false;
+                break;
+            }
+        }
+
+        if (answer == true)
+            cout << "Yes" << endl;
+        else
+            cout << "No" << endl;
+    }
+
+
+    return 0;
+}
+
---
+++ kyopro-tessoku/5.1/B26.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int N = 0;
+
+
+    cin >> N;
+
+
+    vector<bool> Deleted_number(N + 1, false); //true:delete
+    //vector<int> prime_number;
+
+
+    for (int i = 2; i * i <= N; i++) {
+
+        if (Deleted_number[i] == true)continue;
+        for (int j = i * 2; j <= N; j += i) {
+            if (Deleted_number[j] == false)Deleted_number[j] = true;
+        }
+    }
+
+    for (int i = 2; i <= N; i++) {
+        if (Deleted_number[i] == false)cout << i << endl;
+    }
+
+    return 0;
+}
+
