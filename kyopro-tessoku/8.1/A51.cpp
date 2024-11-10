13af04da6f24a68a73c300c73ce0343b0826c46a
Kazu-SK <sonodans109@gmail.com>
2024-11-10 12:19:52 +0900

    add tessoku 8.1

---
+++ kyopro-tessoku/8.1/A51.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    int Q = 0;
+
+
+    cin >> Q;
+
+
+    vector<int> query(Q, 0);
+    stack<string> S;
+    string book_name;
+
+    const int push = 1;
+    const int top = 2;
+    const int del = 3;
+
+
+    for (int i = 0; i < Q; i++) {
+        cin >> query[i];
+
+
+        if (query[i] == push) {
+            cin >> book_name;
+            S.push(book_name);
+        }
+
+        if (query[i] == top) {
+            cout << S.top() << endl;
+        }
+
+        if (query[i] == del) {
+            S.pop();
+        }
+    }
+
+    return 0;
+}
+
---
+++ kyopro-tessoku/8.1/B51.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+	string S;
+	stack<char> query_brackets;
+	stack<int> query_num;
+
+
+	cin >> S;
+
+
+
+	for (int i = 0; i < S.size(); i++) {
+
+		if (query_brackets.empty() == false && query_brackets.top() == '(' && S[i] == ')') {
+
+			cout << query_num.top() << " " << i + 1 << endl;
+			query_brackets.pop();
+			query_num.pop();
+		}
+		else {
+			query_brackets.push(S[i]);
+			query_num.push(i + 1);
+		}
+
+
+	}
+
+
+	return 0;
+}
+
