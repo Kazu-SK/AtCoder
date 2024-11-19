27f3bc829e3e7290f49067a8afc265a3f2fdca08
Kazu-SK <sonodans109@gmail.com>
2024-11-19 14:58:45 +0900

    add abc032 C

---
+++ abc/abc1-100/ABC032/C_2.cpp
@@ -0,0 +1,55 @@
+﻿
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int N = 0;
+    long long K = 0;
+
+    cin >> N >> K;
+
+    vector<long long> S(N + 1, 0);
+    bool zero = false;
+
+    for (int i = 1; i <= N; i++) {
+        cin >> S[i];
+
+        if (S[i] == 0) {
+            zero = true;
+        }
+    }
+
+    if (zero) {
+        cout << N << endl;
+        return 0;
+    }
+
+
+    int right = 1;
+    long long multi = 1;
+    int ans_length = 0;
+
+    for (int left = 1; left <= N; left++) {
+
+        if (left > right)
+            right = left;
+
+        while (right < N && multi * S[right] <= K) {
+            multi *= S[right];
+            right++;
+        }
+
+        ans_length = max(ans_length, right - left);
+
+        if(S[left] != 0 && multi != 1)
+            multi = multi / S[left];
+    }
+
+    cout << ans_length << endl;
+
+    return 0;
+}
+
