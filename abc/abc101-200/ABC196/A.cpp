46486d577b906f2240eb8fca2ed970fc68941339
Kazu-SK <sonodans109@gmail.com>
2024-11-21 08:28:59 +0900

    add abc196 A B

---
+++ abc/abc101-200/ABC196/A.cpp
@@ -0,0 +1,34 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+int main()
+{
+	int num[4] = { 0 };
+	int ans = -1e7;
+
+
+	for (int i = 0; i < 4; i++) {
+		cin >> num[i];
+	}
+
+	for (int i = 0; i < 2; i++) {
+		for (int j = 2; j < 4; j++) {
+			ans = max(ans, num[i] - num[j]);
+		}
+	}
+
+	cout << ans << endl;
+
+
+	return 0;
+}
+
---
+++ abc/abc101-200/ABC196/B.cpp
@@ -0,0 +1,32 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+int main()
+{
+	string S;
+
+
+	cin >> S;
+
+
+	for (int i = 0; i < S.size(); i++) {
+		if (S[i] != '.') {
+			cout << S[i];
+		}
+		else {
+			break;
+		}
+	}
+
+	return 0;
+}
+
