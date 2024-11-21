a1abb0a841185aecadb35a98c817f6fa4aef20fe
Kazu-SK <sonodans109@gmail.com>
2024-11-22 08:28:45 +0900

    add abc196 C

---
+++ abc/abc101-200/ABC196/C.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+int main(){
+
+	LL N;
+
+
+	cin >> N;
+
+	for(LL i = 1; i <= 1e7 ; i++){
+
+		if (stoll(to_string(i) + to_string(i)) > N) {
+			cout << i - 1 << endl;
+			return 0;
+		}
+	}
+}
