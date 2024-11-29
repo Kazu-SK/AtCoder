7c12c879bf22987571b17857e5ba6f78a403ed12
Kazu-SK <sonodans109@gmail.com>
2024-11-29 23:01:18 +0900

    add ABC192 A B

---
+++ abc/abc101-200/ABC192/A.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+int main()
+{
+	int X = 0;
+
+
+	cin >> X;
+
+
+	int ans = 100 - (X % 100);
+
+	cout << ans << endl;
+
+
+	return 0;
+}
+
---
+++ abc/abc101-200/ABC192/B.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+int main()
+{
+	string S;
+	bool judge = true;
+
+
+	cin >> S;
+
+
+	for (int i = 0; i < S.size(); i++) {
+
+		int num = 0;
+
+		if ((i + 1) % 2 == 1)
+			num = S[i] - 'a';
+		else 
+			num = S[i] - 'A';
+
+		if (num >= 0 && num <= 25) {
+			judge = true;
+		}
+		else {
+			judge = false;
+			break;
+		}
+	}
+
+	if (judge)
+		cout << "Yes" << endl;
+	else
+		cout << "No" << endl;
+
+	return 0;
+}
+
