8ee88b038bcb211b6b52b2998bd00b1a24ae8c88
Kazu-SK <sonodans109@gmail.com>
2024-11-29 23:17:53 +0900

    add ABC192 C

---
+++ abc/abc101-200/ABC192/C.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+int main()
+{
+	string N;
+	int K = 0;
+	LL ans = 0;
+	LL llsN = 0;
+	LL lldN = 0;
+
+	cin >> N >> K;
+
+	ans = stoll(N);
+
+	for (int i = 0; i < K; i++) {
+
+		sort(N.begin(), N.end());
+		llsN = stoll(N);
+
+		sort(N.rbegin(), N.rend());
+		lldN = stoll(N);
+
+		ans = lldN - llsN;
+
+		N = to_string(ans);
+	}
+
+	cout << ans << endl;
+
+	return 0;
+}
+
