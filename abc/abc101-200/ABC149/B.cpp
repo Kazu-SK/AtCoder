e8da4176a3f8e284495d3db18e017b797c6de0ea
Kazu-SK <sonodans109@gmail.com>
2024-12-01 08:16:56 +0900

    add ABC149 B

---
+++ abc/abc101-200/ABC149/B.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+int main()
+{
+	LL A = 0, B = 0, K = 0;
+	LL ans_A = 0;
+	LL ans_B = 0;
+
+
+	cin >> A >> B >> K;
+
+
+	ans_A = max((LL)0, A - K);
+	K = max((LL)0, K - A);
+
+	ans_B = max((LL)0, B - K);
+
+	cout << ans_A << " " << ans_B << endl;
+
+	return 0;
+}
+
