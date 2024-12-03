866e83aa4fc3a03877ee28854f58ef01e80d4d30
Kazu-SK <sonodans109@gmail.com>
2024-12-03 22:31:21 +0900

    add mathalgorithm-book prob2-2-4

---
+++ math-algorithm-book/chap2-2/prob2-2-4.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+int main()
+{
+    int N = 0;
+
+
+    cin >> N;
+
+    vector<int> a(N);
+    int sum = 0;
+
+
+    for (int i = 0; i < N; i++) {
+        cin >> a[i];
+        sum += a[i];
+    }
+
+    cout << sum % 100 << endl;
+
+    return 0;
+}
+
