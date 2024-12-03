47cc6f80628d68e48694f9f1288e7624c6fba7cd
Kazu-SK <sonodans109@gmail.com>
2024-12-03 22:19:23 +0900

    add mathalgorithm-book prob2-1-3

---
+++ math-algorithm-book/chap2-1/prob2-1-3.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+int main()
+{
+	int A1, A2, A3;
+
+
+	cin >> A1 >> A2 >> A3;
+
+	cout << A1 * A2 * A3 << endl;
+
+	return 0;
+}
+
