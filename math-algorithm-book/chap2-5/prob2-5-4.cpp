d6bf28d00b361c553e65dc00a56edef7db0fc667
Kazu-SK <sonodans109@gmail.com>
2024-12-05 22:41:31 +0900

    add math-algorithm-book 2-5-4

---
+++ math-algorithm-book/chap2-5/prob2-5-4.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+int main()
+{
+	int N = 0;
+
+
+	cin >> N;
+
+
+	for (int i = 2; i <= N; i++) {
+
+		bool prime = true;
+
+		if (i == 2) {
+			cout << i << " ";
+			continue;
+		}
+
+		for (int j = 2; j <= i / 2; j++) {
+
+			if (i % j == 0) {
+				prime = false;
+				break;
+			}
+		}
+
+		if (prime)
+			cout << i << " ";
+	}
+
+	return 0;
+}
+
