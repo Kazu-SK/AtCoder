5c11953aca80df44356bf852e23389ba5033d558
Kazu-SK <sonodans109@gmail.com>
2024-12-06 23:38:57 +0900

    add math-algorithm-book 3-1-1

---
+++ math-algorithm-book/chap3-1/prob3-1-2.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+int main()
+{
+    LL N = 0;
+
+
+    cin >> N;
+
+    LL init = N;
+
+    for (LL i = 2; i * i <= N; i++) {
+        if (init % i == 0) {
+            init /= i;
+            cout << i << " ";
+            i--;
+        }
+    }
+
+    if (init >= 2) {
+        cout << init << endl;
+    }
+
+    return 0;
+}
+
