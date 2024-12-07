3c3022c91407a3dc736b9eb2839233115a2c2b87
Kazu-SK <sonodans109@gmail.com>
2024-12-07 18:13:00 +0900

    add math-algorithm-book 3-2-1

---
+++ math-algorithm-book/chap3-2/prob3-2-2.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+LL GCD(LL a, LL b) {
+	if (b == 0) return a;
+	else return GCD(b, a % b);
+}
+
+int main()
+{
+	int N = 0;
+
+
+	cin >> N;
+
+
+	vector<LL> A(N);
+
+	for (int i = 0; i < N; i++)
+		cin >> A[i];
+
+	LL ans = A[0];
+
+
+	for (int i = 1; i < N; i++) {
+		ans = GCD(ans, A[i]);
+	}
+
+	cout << ans << endl;
+
+	return 0;
+}
+
