029e40f311145905b1378f6d2eac0c679337a219
Kazu-SK <sonodans109@gmail.com>
2024-12-08 11:21:06 +0900

    add abc382 A B

---
+++ contest/abc/abc301-400/ABC382/A.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+int main()
+{
+	int N = 0;
+	int D = 0;
+
+
+	cin >> N >> D;
+
+	vector<char> S(N + 1);
+
+	int kara_count = 0;
+
+	for (int i = 1; i <= N; i++) {
+		cin >> S[i];
+
+		if (S[i] == '.')
+			kara_count++;
+	}
+
+	cout << kara_count + D << endl;
+
+	return 0;
+}
+
---
+++ contest/abc/abc301-400/ABC382/B.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+int main()
+{
+	int N = 0;
+	int D = 0;
+
+
+	cin >> N >> D;
+
+	vector<char> S(N + 1);
+
+
+	for (int i = 1; i <= N; i++) {
+		cin >> S[i];
+	}
+
+	for (int i = N; i >= 1; i--) {
+		if (S[i] == '@') {
+			S[i] = '.';
+			D--;
+		}
+
+		if (D == 0)
+			break;
+	}
+
+	for (int i = 1; i <= N; i++) {
+		cout << S[i];
+	}
+
+	return 0;
+}
+
