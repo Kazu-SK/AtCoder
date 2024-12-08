d8689bf7e31c5a4cae8ae7d3af5cfbce454b9f36
Kazu-SK <sonodans109@gmail.com>
2024-12-08 12:04:10 +0900

    add math-algorithm-book 3-3-3

---
+++ learning resource/math-algorithm-book/chap3-3/prob3-3-3.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+
+LL CalcC(LL n, LL r) {
+	LL P = 1;
+	LL r_kai = 1;
+	LL ans = 0;
+
+	LL i_lim = max(n - (n - r), n - r) + 1;
+
+	for (LL i = n; i >= i_lim; i--) {
+		P *= i;
+	}
+
+	r = min(r, n - r);
+	for (LL i = r; i >= 1; i--) {
+		r_kai *= i;
+	}
+
+	ans = P / r_kai;
+
+	return ans;
+}
+
+int main()
+{
+	LL n = 0;
+	LL r = 0;
+
+
+	cin >> n >> r;
+
+
+	cout << CalcC(n, r) << endl;
+
+
+	return 0;
+}
+
