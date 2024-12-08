fdc6481ec5a1b9a1a1f4d7b9c93273b5a9b3dbba
Kazu-SK <sonodans109@gmail.com>
2024-12-08 11:21:25 +0900

    add abc383 A B

---
+++ contest/abc/abc301-400/ABC383/A.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+int main()
+{
+	int N = 0;
+
+
+	cin >> N;
+
+
+	vector<int> T(N);
+	vector<int> V(N);
+
+	for (int i = 0; i < N; i++) {
+		cin >> T[i];
+		cin >> V[i];
+	}
+
+
+	int water = 1;
+	int t_itr = 0;
+
+	for (int t = T[0]; t <= T[N - 1]; t++) {
+
+		if(water > 0)
+			water--;
+
+		if (t == T[t_itr]) {
+			water += V[t_itr];
+			t_itr++;
+		}
+	}
+
+	cout << water << endl;
+
+	return 0;
+}
+
---
+++ contest/abc/abc301-400/ABC383/B.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+int main()
+{
+	int H = 0, W = 0, D = 0;
+
+
+	cin >> H >> W >> D;
+
+
+	vector<vector<char>> S(H + 1, vector<char>(W + 1, '.'));
+
+	vector<int> yuka_h;
+	vector<int> yuka_w;
+
+	for (int h = 1; h <= H; h++) {
+		for (int w = 1; w <= W; w++) {
+			cin >> S[h][w];
+
+			if (S[h][w] == '.') {
+				yuka_h.push_back(h);
+				yuka_w.push_back(w);
+			}
+		}
+	}
+
+	int yuka_size = yuka_h.size();
+	int tmp = 0;
+	int ans = 0;
+
+	for (int i = 0; i < yuka_size - 1; i++) {
+		for (int j = i + 1; j < yuka_size; j++) {
+			
+			for (int h = 1; h <= H; h++) {
+				for (int w = 1; w <= W; w++) {
+
+					int distance[2];
+
+					distance[0] = abs(h - yuka_h[i]) + abs(w - yuka_w[i]);
+					distance[1] = abs(h - yuka_h[j]) + abs(w - yuka_w[j]);
+
+					if (distance[0] <= D || distance[1] <= D) {
+						if (S[h][w] == '.') {
+							tmp++;
+						}
+					}
+				}
+			}
+
+			ans = max(ans, tmp);
+			tmp = 0;
+		}
+	}
+
+	cout << ans << endl;
+
+	return 0;
+}
+
