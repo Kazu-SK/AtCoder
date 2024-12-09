da282be22247213d3025b84f3bead2afde761ecc
Kazu-SK <sonodans109@gmail.com>
2024-12-09 22:54:28 +0900

    add math-algorithm-book 3-4-3

---
+++ learning resource/math-algorithm-book/chap3-4/prob3-4-3.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+int main()
+{
+	int N = 0;
+
+
+	cin >> N;
+
+	vector<int> A(N);
+	vector<int> B(N);
+
+
+	for (int i = 0; i < N; i++)
+		cin >> A[i];
+	for (int i = 0; i < N; i++)
+		cin >> B[i];
+
+
+	double expected_value = 0.0;
+
+	for (int i = 0; i < N; i++) {
+		expected_value += (double)A[i] / 3;
+		expected_value += (double)B[i] / 3 * 2;
+	}
+
+	printf("%.3f", expected_value);
+
+	return 0;
+}
+
