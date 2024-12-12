743dace93a87df7d029a37cc66980ee8a02ca54d
Kazu-SK <sonodans109@gmail.com>
2024-12-12 23:12:31 +0900

    add math-algorithm-book 3-4-4

---
+++ learning resource/math-algorithm-book/chap3-4/prob3-4-4.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+int main()
+{
+	int N = 0;
+	double Answer = 0.0;
+
+
+	cin >> N;
+
+
+	for (int i = 1; i <= N; i++) {
+		Answer += 1.0 * N / i;
+	}
+
+	printf("%.12f\n", Answer);
+
+	return 0;
+}
+
