bbebd6288dfb7aacb36fd496fe4a6cf42234581f
Kazu-SK <sonodans109@gmail.com>
2024-12-18 23:21:47 +0900

    add math-algorithm-book 5-4-2

---
+++ learning resource/math-algorithm-book/chap5-4/prob5-4-2.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+int main()
+{
+	int H = 0, W = 0;
+
+
+	cin >> H >> W;
+
+	vector<vector<int>> A(H, vector<int>(W, 0));
+	vector<vector<int>> ans(H, vector<int>(W, 0));
+	vector<int> sum_H(W, 0);
+	vector<int> sum_W(H, 0);
+
+	for (int h = 0; h < H; h++) {
+		for (int w = 0; w < W; w++) {
+			cin >> A[h][w];
+		}
+	}
+
+	for (int h = 0; h < H; h++) {
+		for (int w = 0 ; w < W; w++) {
+			sum_W[h] += A[h][w];
+		}
+	}
+
+	for (int w = 0 ; w < W; w++) {
+		for (int h = 0; h < H; h++) {
+			sum_H[w] += A[h][w];
+		}
+	}
+
+	for (int h = 0; h < H; h++) {
+		for (int w = 0 ; w < W; w++) {
+			ans[h][w] = sum_W[h] + sum_H[w] - A[h][w];
+			cout << ans[h][w] << " ";
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
+
