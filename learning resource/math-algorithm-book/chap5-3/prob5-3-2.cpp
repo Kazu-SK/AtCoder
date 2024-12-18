0106f04bb781865f0ed2455123fcf178198222f0
Kazu-SK <sonodans109@gmail.com>
2024-12-18 22:59:49 +0900

    add math-algorithm-book 5-3-2

---
+++ learning resource/math-algorithm-book/chap5-3/prob5-3-2.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+int main()
+{
+	int ans = 0;
+
+
+	for (int num = 0; num < (1 << 10); num++) {
+
+		int sum = 0;
+		
+		for (int bit = 0; bit < 10; bit++) {
+			if (num & (1 << bit)) {
+				sum += (bit + 1);
+			}
+		}
+
+		if (sum % 2)
+			ans++;
+	}
+
+	cout << ans << endl;
+
+	return 0;
+}
+
