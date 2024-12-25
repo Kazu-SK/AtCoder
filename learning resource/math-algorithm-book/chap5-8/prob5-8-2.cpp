c2b868ba7aaeb1ae05b654928c86a050728b7b43
Kazu-SK <sonodans109@gmail.com>
2024-12-25 22:15:33 +0900

    add math-algorithm-book 5-8-2

---
+++ learning resource/math-algorithm-book/chap5-8/prob5-8-2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+int main()
+{
+	LL N = 0;
+	LL ans = 0;
+
+
+	cin >> N;
+
+
+	if (N % 2 == 0) {
+		ans = N * (N / 2 - 1) + (N / 2);
+	}
+	else {
+		ans = N * ((N - 1) / 2);
+	}
+
+	cout << ans << endl;
+
+	return 0;
+}
+
