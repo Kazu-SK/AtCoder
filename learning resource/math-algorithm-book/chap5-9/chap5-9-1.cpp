f835be4732e25022b9deeb29404f0c643f4bcedf
Kazu-SK <sonodans109@gmail.com>
2024-12-25 22:38:31 +0900

    add math-algorithm-book 5-9-1

---
+++ learning resource/math-algorithm-book/chap5-9/chap5-9-1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+int main()
+{
+	int N = 0;
+	const int YUKICHI = 10000;
+	const int ICHIYO = 5000;
+	const int HIDEYO = 1000;
+	int ans = 0;
+	int bill[3] = { YUKICHI, ICHIYO, HIDEYO };
+
+
+	cin >> N;
+
+
+	for (int i = 0; i < 3; i++) {
+		ans += N / bill[i];
+		N %= bill[i];
+	}
+
+	cout << ans << endl;
+
+	return 0;
+}
+
