04001cdecbd7399d4f10b99f928887b5f34a4687
SK <sonodans109@gmail.com>
2024-12-30 19:03:55 +0900

    test

---
+++ learning resource/test/Test.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+int main()
+{
+    cout << "test" << endl;
+
+    return 0;
+}
+
