49efb107e082ae72b411698f6315d016e7434662
Kazu-SK <sonodans109@gmail.com>
2025-01-05 11:42:55 +0900

    add ABC387 A

---
+++ contest/abc/abc301-400/ABC387/A.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+int main()
+{
+	LL A, B;
+	LL ans = 0;
+
+
+	cin >> A >> B;
+
+	ans = (A + B) * (A + B);
+
+	cout << ans << endl;
+
+	return 0;
+}
+
