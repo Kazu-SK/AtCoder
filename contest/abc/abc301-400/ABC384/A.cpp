d89479a999952701fae1155ef9be90ba4094e705
Kazu-SK <sonodans109@gmail.com>
2025-01-06 22:42:00 +0900

    add ABC384 A

---
+++ contest/abc/abc301-400/ABC384/A.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+int main()
+{
+	int N = 0;
+	char c[2];
+
+
+	cin >> N;
+
+	vector<char> S(N);
+
+	cin >> c[0] >> c[1];
+
+	for (int i = 0; i < N; i++)
+		cin >> S[i];
+
+	for (int i = 0; i < N; i++) {
+		if (S[i] != c[0])
+			S[i] = c[1];
+	}
+
+	for (int i = 0; i < N; i++) {
+		cout << S[i];
+	}
+
+	return 0;
+}
+
