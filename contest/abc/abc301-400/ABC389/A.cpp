d4e55f9dd78555513a470195f5ee3cd153fa2cfe
SK <sonodans109@gmail.com>
2025-01-19 17:52:37 +0900

    add ABC389 A

---
+++ contest/abc/abc301-400/ABC389/A.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+int main()
+{
+	string S;
+	int num[2];
+
+
+	cin >> S;
+
+	num[0] = S[0] - '0';
+	num[1] = S[2] - '0';
+
+	cout << num[0] * num[1] << endl;
+
+	return 0;
+}
+
