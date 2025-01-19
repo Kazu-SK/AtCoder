3e4f689913d818c6cac83e091d7c94325a295ff9
SK <sonodans109@gmail.com>
2025-01-19 16:37:30 +0900

    add ABC388 B

---
+++ contest/abc/abc301-400/ABC388/B.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+int main()
+{
+    int N = 0, D = 0;
+
+
+    cin >> N >> D;
+
+    vector<int> T(N + 1);
+    vector<int> L(N + 1);
+    vector<int> H(D + 1, 0);
+
+
+    for (int n = 1; n <= N; n++) {
+        cin >> T[n] >> L[n];
+    }
+
+    for (int d = 1; d <= D; d++) {
+        for (int n = 1; n <= N; n++) {
+            H[d] = max(H[d], T[n] * (L[n] + d));
+        }
+    }
+
+    for (int d = 1; d <= D; d++) {
+        cout << H[d] << endl;
+	}
+
+    return 0;
+}
+
