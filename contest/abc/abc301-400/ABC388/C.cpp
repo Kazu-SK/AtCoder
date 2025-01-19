5eb155f080796ddd4cefd4d2cf47981e4ca4a575
SK <sonodans109@gmail.com>
2025-01-19 16:38:10 +0900

    add ABC388 C

---
+++ contest/abc/abc301-400/ABC388/C.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+int main()
+{
+	int N = 0;
+
+
+	cin >> N;
+
+	vector<LL> A(N + 1);
+
+	for (int i = 1; i <= N; i++) {
+		cin >> A[i];
+	}
+
+	int j = 2; 
+	LL ans = 0;
+
+	for (int i = 1; i <= N; i++) {
+
+		while (A[i] * 2 > A[j] && j < N)j++;
+
+		if(A[i] * 2 <= A[j])
+			ans += N - j + 1;
+	}
+
+	cout << ans << endl;
+
+
+	return 0;
+}
+
