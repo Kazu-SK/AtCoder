14fc2eb0d4841d617084fa937fd3fd8359880e08
SK <sonodans109@gmail.com>
2025-01-19 17:53:02 +0900

    add ABC389 B

---
+++ contest/abc/abc301-400/ABC389/B.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+int main()
+{
+	LL X = 0;
+	LL ans = 1;
+
+
+	cin >> X;
+
+
+	int N = 1;
+	while (true) {
+		
+		ans *= N;
+
+		if (ans == X)
+			break;
+
+		N++;
+	}
+
+	cout << N << endl;
+
+	return 0;
+}
+
