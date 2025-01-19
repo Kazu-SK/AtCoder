33009d3809d5fce9f174b4016112f120fc54580d
SK <sonodans109@gmail.com>
2025-01-19 16:36:54 +0900

    add ABC388 A

---
+++ contest/abc/abc301-400/ABC388/A.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+int main()
+{
+	string S;
+	string ans;
+	const string UPC = "UPC";
+
+
+	cin >> S;
+
+	ans = S[0] + UPC;
+
+
+	cout << ans << endl;
+
+	return 0;
+}
+
