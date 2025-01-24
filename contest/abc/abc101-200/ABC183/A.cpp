2dbddbf80661b7ad6f2685a6612e22b9abde3eaa
SK <sonodans109@gmail.com>
2025-01-24 20:57:01 +0900

    add ABC183 A B

---
+++ contest/abc/abc101-200/ABC183/A.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+int main()
+{
+	int x = 0;
+
+
+	cin >> x;
+
+	if (x >= 0)
+		cout << x << endl;
+	else
+		cout << 0 << endl;
+
+	return 0;
+}
+
---
+++ contest/abc/abc101-200/ABC183/B.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+
+
+int main()
+{
+	double Sx = 0, Sy = 0;
+	double Gx = 0, Gy = 0;
+	double a = 0;
+	double b = 0;
+	double ans = 0;
+
+
+	cin >> Sx >> Sy >> Gx >> Gy;
+
+	Gy *= -1;
+
+	a = (Gy - Sy) / (Gx - Sx);
+	b = Gy - a * Gx;
+
+	ans = -b / a;
+
+	printf("%.6f\n", ans);
+
+	return 0;
+}
+
