c476ad63d461c7e4bdd81bc0313f81be467f25f5
SK <sonodans109@gmail.com>
2025-02-11 22:04:48 +0900

    add ABC390 A

---
+++ contest/abc/abc301-400/ABC390/A.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+int main()
+{
+	vector<int> A(6);
+
+
+	for (int i = 1; i <= 5; i++)
+		cin >> A[i];
+
+	bool judge = false;
+
+	for (int i = 1; i <= 4; i++) {
+		if (A[i] > A[i + 1]) {
+			swap(A[i], A[i + 1]);
+			judge = true;
+			break;
+		}
+	}
+
+
+	for (int i = 1; i <= 5; i++) {
+		if (A[i] != i)
+			judge = false;
+	}
+
+	if (judge)
+		cout << "Yes" << endl;
+	else
+		cout << "No" << endl;
+
+
+	return 0;
+}
+
