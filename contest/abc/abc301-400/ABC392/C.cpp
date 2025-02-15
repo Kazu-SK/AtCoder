ec337a0ebb708235dbed4beabc1a5ba929a50eaf
SK <sonodans109@gmail.com>
2025-02-15 09:32:48 +0900

    add ABC392 C

--- contest/abc/abc301-400/ABC392/C.cpp
+++ contest/abc/abc301-400/ABC392/C.cpp
@@ -11,38 +11,38 @@ using namespace std;
 using LL = long long;
 
 struct Info {
-    int P;
-    int Q;
+	int P;
+	int Q;
 };
 
 int main()
 {
-    int N = 0;
+	int N = 0;
 
 
-    cin >> N;
+	cin >> N;
 
-    vector<Info> info(N + 1);
-    map<int, int> Map;
+	vector<Info> info(N + 1);
+	map<int, int> Map;
 
-    for (int i = 1; i <= N; i++)
-        cin >> info[i].P;
+	for (int i = 1; i <= N; i++)
+		cin >> info[i].P;
 
-    for (int i = 1; i <= N; i++)
-        cin >> info[i].Q;
+	for (int i = 1; i <= N; i++)
+		cin >> info[i].Q;
 
-    for (int i = 1; i <= N; i++) {
-        Map[i] = info[i].Q;
-    }
+	for (int i = 1; i <= N; i++) {
+		Map[i] = info[i].Q;
+	}
 
-    sort(info.begin(), info.end(), [&](const auto& l, const auto& r) {
-        return l.Q < r.Q;
+	sort(info.begin(), info.end(), [&](const auto& l, const auto& r) {
+		return l.Q < r.Q;
 	});
 
-    for (int i = 1; i <= N; i++) {
-        cout << Map[info[i].P] << " ";
-    }
+	for (int i = 1; i <= N; i++) {
+		cout << Map[info[i].P] << " ";
+	}
 
-    return 0;
+	return 0;
 }
 
