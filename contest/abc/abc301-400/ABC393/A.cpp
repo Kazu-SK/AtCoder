3d3f396fd221f74a030a5fe408b8f8ed6e4fce17
SK <sonodans109@gmail.com>
2025-02-18 22:25:07 +0900

    add ABC393 A

---
+++ contest/abc/abc301-400/ABC393/A.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+int main()
+{
+	string S[2];
+	int ans = 0;
+
+
+	cin >> S[0] >> S[1];
+
+	if (S[0] == "sick" && S[1] == "sick")
+		ans = 1;
+	else {
+		if (S[0] == "sick" && S[1] == "fine")
+			ans = 2;
+		else {
+			if (S[0] == "fine" && S[1] == "sick")
+				ans = 3;
+			else 
+				ans = 4;
+		}
+	}
+
+	cout << ans << endl;
+
+	return 0;
+}
+
