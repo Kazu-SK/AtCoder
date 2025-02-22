edbda414f958699ccddbf82a3812e3c446c828fe
SK <sonodans109@gmail.com>
2025-02-22 22:35:31 +0900

    add ABC394 A

---
+++ contest/abc/abc301-400/ABC394/A.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+int main()
+{
+	string S;
+	string ans;
+
+
+	cin >> S;
+
+
+	for (int i = 0; i < S.size(); i++) {
+		if (S[i] == '2')
+			ans.push_back(S[i]);
+	}
+
+	cout << ans << endl;
+
+	return 0;
+}
+
