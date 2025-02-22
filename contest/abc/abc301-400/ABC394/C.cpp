ab4f5fc6d4b476e2ba8b99fc2592e4f133046a2f
SK <sonodans109@gmail.com>
2025-02-22 22:36:45 +0900

    add ABC394 C

---
+++ contest/abc/abc301-400/ABC394/C.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using LL = long long;
+
+int main()
+{
+	string S;
+
+
+	cin >> S;
+
+
+	int i = S.size() - 1;
+
+	while(i >= 1){
+		if (S[i -  1] == 'W' && S[i] == 'A') {
+
+			S[i] = 'C';
+			S[i - 1] = 'A';
+		}
+		else {
+		}
+
+		i--;
+
+	}
+
+	cout << S << endl;
+
+	return 0;
+}
+
