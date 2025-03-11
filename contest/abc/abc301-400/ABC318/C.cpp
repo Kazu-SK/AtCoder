8552ba948d35578ac0afefb0a7e4c77952e5330d
SK <sonodans109@gmail.com>
2025-03-11 22:54:06 +0900

    add ABC318 C

---
+++ contest/abc/abc301-400/ABC318/C.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <set>
+
+using namespace std;
+using llong = long long;
+
+int main()
+{
+    int N = 0;
+    int D = 0;
+    llong P = 0;
+
+
+    cin >> N >> D >> P;
+
+
+    vector<llong> F(N + 1, 0);
+
+    for (int i = 1; i <= N; i++)
+        cin >> F[i];
+
+    sort(F.rbegin(), F.rend());
+
+    llong sum = 0;
+    llong ans = 0;
+    int i_count = 0;
+
+    for (int i = 0; i < N; i++) {
+        sum += F[i];
+
+        i_count++;
+
+        if (i_count == D) {
+            if (P < sum) 
+                sum = P;
+
+            ans += sum;
+            i_count = 0;
+            sum = 0;
+        }
+        else if (i == N - 1) {
+            if (P < sum) 
+                sum = P;
+
+            ans += sum;
+        }
+        else {
+        }
+
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
+
